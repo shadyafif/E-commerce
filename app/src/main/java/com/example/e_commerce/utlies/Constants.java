1d138084ca5e7ac86feb4d6b762696a2cb47193f
shadyafif <51825486+shadyafif@users.noreply.github.com>
2021-04-08 16:30:26 +0200

    Update Constants.java

--- app/src/main/java/com/example/e_commerce/utlies/Constants.java
+++ app/src/main/java/com/example/e_commerce/utlies/Constants.java
@@ -1,8 +1,8 @@
 package com.example.e_commerce.utlies;
 
 public class Constants {
-    public static final String CONSUMER_KEY = "ck_33e5438f365cfdce74b96104330823a3c8f11eac";
-    public static final String SECRET_KEY = "cs_a25798bfab1a7aa6207717fc07034e79c77f5cf9";
+    public static final String CONSUMER_KEY = "ck_9399457c61678714fc0662c50e895caa999ed70e";
+    public static final String SECRET_KEY = "cs_21006b33712e0de9fd31496e0dc76f9334f65271";
     public static String FACEBOOK_URL = "https://www.facebook.com/SemicolonStore/";
     public static String FACEBOOK_PAGE_ID = "Shady Profile";
     public static String Twitter_URL = "https://twitter.com/edent";
